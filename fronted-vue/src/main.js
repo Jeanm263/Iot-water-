547f0923ddf4555a48b24a23e0e831a7fca7691e
Jean263 <jpv.mora@yavirac.edu.ec>
2025-01-21 16:21:14 -0500

    Cambios

--- fronted-vue/src/main.js
+++ fronted-vue/src/main.js
@@ -5,19 +5,24 @@ import router from './router';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import axios from 'axios';
 
-// Configurar Axios para incluir el token en todas las solicitudes
-axios.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;// Uso correcto de los backticks
+// Verificar la existencia de localStorage antes de configurar Axios
+if (typeof localStorage !== 'undefined') {
+  // Configurar Axios para incluir el token en todas las solicitudes
+  axios.interceptors.request.use(
+    config => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`; // Uso correcto de los backticks
+      }
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
     }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
+  );
+} else {
+  console.error('localStorage no está disponible');
+}
 
 // Crear la aplicación Vue
 const app = createApp(App);
